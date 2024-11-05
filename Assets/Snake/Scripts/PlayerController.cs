ec520793eab09c49f6f5ad07b891ea979cdf47eb
deepanshu <deepanshu120100@gmail.com>
2024-11-04 19:36:49 -0700

    Update2v
    +snake body rotation fixed
    +body addition fixed
    +movement fix

--- Assets/Snake/Scripts/PlayerController.cs
+++ Assets/Snake/Scripts/PlayerController.cs
@@ -19,10 +19,14 @@ public class PlayerController : MonoBehaviour
     private List<GameObject> bodyParts = new List<GameObject>();
     private List<Vector3> positionsHistory = new List<Vector3>();
     private Vector2 currentMoveInput; 
-
+    private List<Quaternion> rotationsHistory = new List<Quaternion>();
+    public int maxRotationHistorySize = 10;
+    [SerializeField] private GameObject TailPart;
     private void Start()
     {
         positionsHistory.Add(playerTransform.position);
+        GameObject body = Instantiate(TailPart, playerTransform.position, Quaternion.identity);
+        bodyParts.Insert(0, body);
     }
     private void OnEnable()
     {
@@ -39,6 +43,7 @@ public class PlayerController : MonoBehaviour
         HandleMovement();
         transform.position += transform.forward * (moveSpeed * Time.deltaTime);
         UpdatePositionsHistory();
+        UpdateRotationsHistory();
         MoveBodyParts();
     }
     private void Onjump(bool isJumping)
@@ -72,33 +77,31 @@ public class PlayerController : MonoBehaviour
         if (positionsHistory.Count > maxHistorySize)
             positionsHistory.RemoveAt(positionsHistory.Count - 1);
     }
+    private void UpdateRotationsHistory()
+    {
+        rotationsHistory.Insert(0, transform.rotation);
+
+        if (rotationsHistory.Count > maxRotationHistorySize)
+            rotationsHistory.RemoveAt(rotationsHistory.Count - 1);
+    }
     private void MoveBodyParts()
     {
         for (int i = 0; i < bodyParts.Count; i++)
         {
             int historyIndex = Mathf.Clamp(i * gap, 0, positionsHistory.Count - 1);
+        
             Vector3 targetPosition = positionsHistory[historyIndex];
+            Quaternion targetRotation = rotationsHistory[historyIndex];
 
             bodyParts[i].transform.position = Vector3.Lerp(bodyParts[i].transform.position, targetPosition, bodySpeed * Time.deltaTime);
+            bodyParts[i].transform.rotation = Quaternion.Lerp(bodyParts[i].transform.rotation, targetRotation, bodySpeed * Time.deltaTime);
         }
     }
+    
     public void GrowSnake()
     {
-        GameObject body = Instantiate(bodyPrefab);
-
-        if (bodyParts.Count > 0)
-        {
-            Vector3 lastBodyPosition = bodyParts[bodyParts.Count - 1].transform.position;
-
-            Vector3 directionToNewPart = (lastBodyPosition - playerTransform.position).normalized;
-
-            body.transform.position = lastBodyPosition + directionToNewPart * gap;
-        }
-        else
-        {
-            body.transform.position = playerTransform.position;
-        }
-        bodyParts.Add(body);
+        GameObject body = Instantiate(bodyPrefab, playerTransform.position, Quaternion.identity);
+        bodyParts.Insert(0, body);
     }
     private void OnTriggerEnter(Collider other)
     {
